91720acca8fcc8c7781233f53e7e2218d44ba1e5
Md Moinuddin <moinuddinshekh2@gmail.com>
2021-03-23 20:37:28 +0000

    Signed-off-by: Md Moinuddin <moinuddinshekh2@gmail.com>
    	modified:   line_follower.py
    
    -Changed speed of motor to 50%

--- line_follower.py
+++ line_follower.py
@@ -1,14 +1,25 @@
 import RPi.GPIO as GPIO
 
-#setup
+#setmode
 GPIO.setmode(GPIO.BCM)
 
-#pin_setup
+#pin_setup for motors.
 
 GPIO.setup(17,GPIO.OUT)
 GPIO.setup(27,GPIO.OUT)
 GPIO.setup(22,GPIO.OUT)
 GPIO.setup(18,GPIO.OUT)
+GPIO.setup(25,GPIO.OUT)
+GPIO.setup(8,GPIO.OUT)
+
+pwm_right = GPIO.PWM(25,100)
+pwm_left = GPIO.PWM(8,100)
+pwm_right.start(0)
+pwm_left.start(0)
+
+
+
+#Pin setup for IR sensor.
 
 GPIO.setup(23,GPIO.IN)
 GPIO.setup(24,GPIO.IN)
@@ -19,29 +30,38 @@ def left():
 	GPIO.output(17,GPIO.LOW)
 	GPIO.output(27,GPIO.LOW)
 	GPIO.output(22,GPIO.LOW)
+	pwm_left.ChangeDutyCycle(50)
+	pwm_right.ChangeDutyCycle(50)
 
 def right():
 	GPIO.output(17,GPIO.HIGH)
 	GPIO.output(18,GPIO.LOW)
 	GPIO.output(27,GPIO.LOW)
 	GPIO.output(22,GPIO.LOW)
+	pwm_right.ChangeDutyCycle(50)
+	pwm_left.ChangeDutyCycle(50)
 
 def forward():
 	GPIO.output(18,GPIO.HIGH)
 	GPIO.output(17,GPIO.HIGH)
 	GPIO.output(27,GPIO.LOW)
 	GPIO.output(22,GPIO.LOW)
+	pwm_left.ChangeDutyCycle(50)
+	pwm_right.ChangeDutyCycle(50)
 
 def backward():
+	#Not Currently used.
 	GPIO.output(27,GPIO.HIGH)
 	GPIO.output(22,GPIO.HIGH)
-	
+	pwm_left.ChangeDutyCycle(50)
+	pwm_right.ChangeDutyCycle(50)
+		
 def stop():
 	GPIO.output(18,GPIO.LOW)
 	GPIO.output(17,GPIO.LOW)
 	GPIO.output(27,GPIO.LOW)
 	GPIO.output(22,GPIO.LOW)
-	#GPIO.cleanup()
+	
 while True:
 
 	data_r = GPIO.input(23)
